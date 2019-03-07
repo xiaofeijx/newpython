cd76a046d812219d032495e1ed07b095b7d8dcb0
xiaofeijx <7880774@qq.com>
2019-03-07 16:50:19 +0800

    2019.3.7 16:50 page 129

--- regexp.py
+++ regexp.py
@@ -52,3 +52,60 @@ print(mo1.group())
 
 
 # python默认贪心，匹配更长结果，非贪心模型在花括号后面加上"?"
+greedHaRegex = re.compile(r'(Ha){3,5}|(ha)*')
+mo1 = greedHaRegex.search('HaHaHaHaHaHa')
+print(mo1.group())
+
+
+greedHaRegex = re.compile(r'(Ha){3,5}?')
+mo1 = greedHaRegex.search('HaHaHaHaHaHa')
+print(mo1.group())
+
+# findall方法：没有分组返回一个匹配字符串列表，有分组返回一个元组列表
+phoneNumber = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+mo = phoneNumber.findall('My phone number is 415-555-4242 and cell number is 444-555-6666.')
+print(mo)
+
+phoneNumber = re.compile(r'(\d\d\d)-(\d\d\d)-(\d\d\d\d)')
+mo = phoneNumber.findall('My phone number is 415-555-4242 and cell number is 444-555-6666.')
+print(mo)
+
+# 字符分类  \d \D \w \W \s \S
+xmasRegex = re.compile(r'\d+\s\w+')  #\w+  字母直到空格
+mo = xmasRegex.findall('12 drummers, 11 pipers, 10 lords, 9 ladies, 8 maids, 7 swans, 6 geese, 5 rings, 4 birds, 3 hens, 2 doves, 1 partridge')
+print(mo)
+
+# 通配符 "."通配单位字符,换行符除外
+atReg = re.compile(r'.at')
+mo = atReg.findall('The cat in the hat sat on the flat mat.')
+print(mo)
+
+# (.*)匹配多个字符，贪心模式
+nameRegex = re.compile(r'First Name: (.*) Last Name: (.*)')
+mo = nameRegex.search('First Name: Al Last Name: Sweigart')
+print(mo.group()) #所有匹配
+print(mo.group(1))
+print(mo.group(2))
+print(mo.groups()) #所有分组
+
+# (.*?)匹配多个字符,非贪心模式
+nongreedyRegex = re.compile(r'<.*?>')
+mo = nongreedyRegex.search('<To serve man> for dinner.>')
+print(mo.group())
+
+
+greedyRegex = re.compile(r'<.*>')
+mo = greedyRegex.search('<To serve man> for dinner.>')
+print(mo.group())
+
+
+# re.I  不区分大小写
+robocop = re.compile(r'robocop', re.I)
+mo = robocop.findall('Robocop roBocop is part man, part machine, all cop.')
+print(mo)
+
+namesRegex = re.compile(r'Agent \w+')
+
+agentNamesRegex = re.compile(r'(Agent \w)\w*')
+x = agentNamesRegex.sub(r'\1****', 'Agent Alice told Agent Carol that Agent Eve knew Agent Bob was a double agent.')
+print(x)
